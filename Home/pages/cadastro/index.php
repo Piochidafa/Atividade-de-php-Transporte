783c212c89a6bc5d1592ea365148bb54f3758fa0
piochidafa <cauacastrooliveira@outlook.com>
2023-05-01 22:45:50 -0400

    ADD o Esqueleto da tela de cadastro

---
+++ Home/pages/cadastro/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Cadastro</title>
+</head>
+<body>
+
+    <div class="container">
+        <img  src="./assets/roda.svg" alt="">
+        
+            <div class="regreg">
+                <p class="header">Cadastro API-Tranporte</p>
+            <form class="form" action="">
+                <div class="campo">
+                    <label for="">E-mail</label>
+                    <input class="vali" type="email">
+                </div>
+                <div class="campo">
+                    <label for="">Senha</label>
+                    <input class="vali" type="password">
+                </div>
+                <div class="campo">
+                    <label for="">Digite sua senha novamente</label>
+                    <input class="vali" type="password">
+                </div>
+                <input class="butt" type="submit" value="Cadastrar" >
+            </form>
+
+
+    </div>
+
+</div>
+    
+</body>
+</html>
